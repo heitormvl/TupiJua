33bd444d2b0988cb7ebcc2b973bb939c0241759d
heitormvl <heitormvl12@gmail.com>
2026-01-15 13:16:02 -0300

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TupiJua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
